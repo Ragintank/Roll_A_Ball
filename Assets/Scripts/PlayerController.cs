8b474d3207852e715c12dd6e8bbd3cecbcc13ee5
TylerLucas <tjlucas519@outlook.com>
2022-10-25 12:58:55 -0500

    Roll-a-Ball Tutorial, Displaying Score and Text. This did not go as far as I thought it would, messaged Zac to send me Rolly Ball project, so that I can expand upon that.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,26 +10,42 @@ public class PlayerController : MonoBehaviour
 	private float movementX;
 	private float movementY;
 	public float speed;
+
+	private int count = 0;
+	public TextMeshProUGUI countText;
+	public GameObject winTextObject;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+		SetCountText();
+		winTextObject.SetActive(false);
     }
 	private void FixedUpdate()
 	{
 		Vector3 movement = new Vector3(movementX, 0f, movementY);
 		rb.AddForce(movement * speed);
 	}
-	void OnMove(InputValue movementValue)
-	{
-        Vector2 movementVector = movementValue.Get<Vector2>();
-		movementX = movementVector.x;
-		movementY = movementVector.y;
-	}
 	private void OnTriggerEnter(Collider other)
 	{
 		if(other.gameObject.CompareTag("Collectable"))
 		{
 			other.gameObject.SetActive(false);
+			count++;
+			SetCountText();
+		}
+	}
+	void OnMove(InputValue movementValue)
+	{
+		Vector2 movementVector = movementValue.Get<Vector2>();
+		movementX = movementVector.x;
+		movementY = movementVector.y;
+	}
+	void SetCountText()
+	{
+		countText.text = "Gold: " + count.ToString();
+		if(count >= 4)
+		{
+			winTextObject.SetActive(true);
 		}
 	}
 }
